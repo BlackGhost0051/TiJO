362262b207febad5b9bb234ffe9c0e3de66c369a
BlackGhost0051 <147267828+BlackGhost0051@users.noreply.github.com>
2025-02-21 09:26:45 +0100

    Mod test_math_lib.py

--- Lab1/test/test_math_lib.py
+++ Lab1/test/test_math_lib.py
@@ -5,16 +5,20 @@ from Lab1.src.math_lib import max
 
 
 # class Test(TestCase):
-def test_max(self) -> None:
+def test_max() -> None:
     list1 = [1,2,3,4,5]
     result = max(list1)
     assert result == 5
 
-def test_max_none(self) -> None:
+def test_max_none() -> None:
     list2 = [2, 3, 5, 7, None]
     result = max(list2)
     assert result == None
 
-def test_is_perfect(self) -> None:
+def test_is_perfect() -> None:
     assert is_perfect(6) == True
     assert is_perfect(1) == False
+
+test_max()
+test_max_none()
+test_is_perfect()
